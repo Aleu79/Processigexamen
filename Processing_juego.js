a6748512b68f8a26762df5ac20e2a71f97910a80
Aleu79 <aleeaguero79@gmail.com>
2024-05-17 03:15:28 +0000

    segundo Francia, segundo commit

--- Processing_juego.js
+++ Processing_juego.js
@@ -1,94 +1,89 @@
-let circulo1, circulo2;
-let rojoColor, azulColor, violetaColor;
-let coloVioleta = false;
+let circuloRojo, circuloAzul;
+let rojoColor, azulColor, violetaColor, negroColor, blancoColor;
 let colision = false;
+let tiempoColision = 0;
 
 function setup() {
   createCanvas(600, 600);
   rojoColor = color(255, 0, 0);
   azulColor = color(0, 0, 255);
   violetaColor = color(148, 0, 211);
-  
-  circulo1 = new Circulo(50, height / 2, 30, rojoColor, 6);
-  circulo2 = new Circulo(550, height / 2, 30, azulColor, 4);
+  negroColor = color(0);
+  blancoColor = color(255);
+
+  circuloRojo = new Circulo(50, height / 2, 30, rojoColor, 8); // Movimiento más rápido
+  circuloAzul = new Circulo(550, height / 2, 30, azulColor, 6); // Movimiento más lento
 }
 
 function draw() {
   background(220);
-  
+
+  // Si hay colisión
   if (colision) {
-    fill(violetaColor);
-    circulo1.display();
-    circulo2.display();
-    if (coloVioleta) {
-      violetaColor.setAlpha(255);
-      coloVioleta = false;
-    } else {
-      violetaColor.setAlpha(100);
-      coloVioleta = true;
-    }
-    setTimeout(() => {
+    // Cambiar los colores a violeta
+    circuloRojo.color = violetaColor;
+    circuloAzul.color = violetaColor;
+
+    // Si ha pasado el tiempo suficiente, restablecer los colores y la colisión
+    if (millis() - tiempoColision > 1500) {
       colision = false;
-      circulo1.color = rojoColor;
-      circulo2.color = azulColor;
-    }, 1500);
-  } else {
-    circulo1.update();
-    circulo2.update();
-    circulo1.display();
-    circulo2.display();
+      circuloRojo.color = rojoColor;
+      circuloAzul.color = azulColor;
+    }
+  } 
+
+  // Actualizar y mostrar círculos
+  circuloRojo.update();
+  circuloAzul.update();
+  circuloRojo.display();
+  circuloAzul.display();
+
+  // Colisión con el techo
+  if (circuloRojo.y - circuloRojo.radius <= 0) {
+    circuloRojo.color = blancoColor; // Cambiar a blanco si colisiona con el techo
   }
-  
-  if (circulo1.y - circulo1.radius <= 0 || circulo1.y + circulo1.radius >= height) {
-    circulo1.changeColor();
+
+  // Colisión con el piso
+  if (circuloAzul.y + circuloAzul.radius >= height) {
+    circuloAzul.color = negroColor; // Cambiar a negro si colisiona con el piso
   }
-  
-  if (circulo2.y - circulo2.radius <= 0 || circulo2.y + circulo2.radius >= height) {
-    circulo2.changeColor();
-  }
-  
-  if (circulo1.intersects(circulo2)) {
+
+  // Colisión entre los círculos
+  if (circuloRojo.intersects(circuloAzul)) {
     colision = true;
+    tiempoColision = millis(); // Guardar el tiempo de la colisión
   }
 }
 
 class Circulo {
-  constructor(x, y, radius, color, rapidez) {
+  constructor(x, y, radius, color, velocidad) {
     this.x = x;
     this.y = y;
     this.radius = radius;
     this.color = color;
-    this.rapidez = rapidez;
-    this.velocidadX = random(-this.rapidez, this.rapidez);
-    this.velocidadY = random(-this.rapidez, this.rapidez);
+    this.velocidad = velocidad;
+    this.velocidadX = random(-this.velocidad, this.velocidad);
+    this.velocidadY = random(-this.velocidad, this.velocidad);
   }
-  
+
   update() {
     this.x += this.velocidadX;
     this.y += this.velocidadY;
-    
+
     if (this.x - this.radius < 0 || this.x + this.radius > width) {
       this.velocidadX *= -1;
     }
-    
+
     if (this.y - this.radius < 0 || this.y + this.radius > height) {
       this.velocidadY *= -1;
     }
   }
-  
+
   display() {
     fill(this.color);
     ellipse(this.x, this.y, this.radius * 2);
   }
-  
-  changeColor() {
-    if (this.color === rojoColor) {
-      this.color = color(255, 165, 0);
-    } else {
-      this.color = color(0, 0, 255);
-    }
-  }
-  
+
   intersects(otro) {
     let d = dist(this.x, this.y, otro.x, otro.y);
     return d < this.radius + otro.radius;
